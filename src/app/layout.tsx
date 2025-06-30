20f3fb2e4ac009ffd76715c27610d0a3525505e9
manik <maniksarker652@gmail.com>
2025-06-30 17:11:19 +0600

    try again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,36 @@
-import LayoutWrapper from '@/components/layoutWrapper/LayoutWrapper'
-import './globals.css'
-import { Geist, Geist_Mono, Poppins } from 'next/font/google'
-import { Toaster } from 'react-hot-toast'
-import { Provider } from 'react-redux'
-import { store } from '@/redux/main/store'
+import LayoutWrapper from "@/components/layoutWrapper/LayoutWrapper";
+import "./globals.css";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import { Provider } from "react-redux";
+import { store } from "@/redux/main/store";
 
 const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-})
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
+
+//
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'Stock Market',
+  title: "Stock Market",
   description:
-    'Stock Market is a platform for people to buy and sell stocks, view market trends and make informed investment decisions.',
-}
+    "Stock Market is a platform for people to buy and sell stocks, view market trends and make informed investment decisions.",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body
@@ -45,13 +43,13 @@ export default function RootLayout({
           toastOptions={{
             style: {
               backgroundColor: `rgba(25, 48, 80, 1)`,
-              color: 'white',
-              borderRadius: '0px',
-              border: '1px solid black',
+              color: "white",
+              borderRadius: "0px",
+              border: "1px solid black",
             },
           }}
         />
       </body>
     </html>
-  )
+  );
 }
